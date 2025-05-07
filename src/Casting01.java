6a834df8768b0d175b4418b3b71d6d5d53d113b8
Ivanortizc <Ivanortizc22@gmail.com>
2025-05-07 13:12:33 -0400

    Finish

--- src/Casting01.java
+++ src/Casting01.java
@@ -1,20 +1,17 @@
 public class Casting01 {
-    public static void main(String[] args) {
-        //Declare and initialize a byte with a value of 128
-        //Observe NetBeans' complaint
+    public static void main(String[]args){
+    short myShort = 128;
+    System.out.println((byte)myShort);
 
-        
-        //Declare and initialize a short with a value of 128
-        //Create a print statement that casts this short to a byte
+    byte maxByte = 127;
 
-        
-        
-        //Declare and initialize a byte with a value of 127
-        //Add 1 to this variable and print it
-        //Add 1 to this variable again and print it again
+    System.out.println(maxByte);
 
-        
-        
-        
-    }    
+    maxByte+=1;
+    System.out.println(maxByte);
+
+    maxByte +=1;
+            System.out.println(maxByte);
+}
+
 }
--- src/Parsing01.java
+++ src/Parsing01.java
@@ -1,18 +1,14 @@
 public class Parsing01 {
     public static void main(String[] args) {
         //Declare and intitialize 3 Strings: shirtPrice, taxRate, and gibberish
-        
-        
-        
-        
-        //Parse shirtPrice and taxRate, and print the total tax
-        
-        
-        
-        //Try to parse taxRate as an int
-        
-        //Try to parse gibberish as an int
-        
+        String shirtPrice = "15";
+        String taxRate = "0.05";
+        String gibberish = "887ds7nds87dsfs";
+        int price = Integer.parseInt(shirtPrice);
+        double tax = Double.parseDouble(taxRate);
+        double totaltax = price * tax;
+        System.out.println("Tax: " + totaltax);
+
     }
-    
 }
+
